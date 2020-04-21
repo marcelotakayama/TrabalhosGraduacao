0eb78500a07f4cfcdefaae29c07473ebd86deda1
marcelotakayama <marcelo.takayama00@gmail.com>
2020-04-20 23:16:14 -0300

    Fibonacci Recursivo

---
+++ terceiroSemestre/Python/fibonacciRecursivo.py
@@ -0,0 +1,5 @@
+def fibonacci(n):
+    if n == 0: return 0
+    elif n == 1: return 1
+    else: return fibonacci(n-1)+fibonacci(n-2)
+print("Valor do Fibonacci: ", fibonacci(int(input("Digite o valor que deseja fazer o Fibonacci: "))))
